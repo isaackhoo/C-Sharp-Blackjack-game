0053b516341b9b9d802ef495740c21fc22e3d3e6
Isaac <isaackhoo@hotmail.sg>
2020-06-03 11:38:30 +0800

    added game ui

--- testCsharp/View/GameView.xaml.cs
+++ testCsharp/View/GameView.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using testCsharp.Model;
+using testCsharp.Model.Decks;
 
 namespace testCsharp.View
 {
@@ -19,22 +22,40 @@ namespace testCsharp.View
     /// </summary>
     public partial class GameView : Page
     {
+        ActiveDeck activeDeck;
+        ObservableCollection<Card> testHand;
         public GameView()
         {
             InitializeComponent();
 
             // hides the back and forward button provided by default
             ShowsNavigationUI = false;
+
+            // hide UI
+            //HitButton.Visibility = Visibility.Hidden;
+            //StayButton.Visibility = Visibility.Hidden;
+
+            GameTextFeedback.Text = "illumina blackjack";
+            GameTextFeedback.Visibility = Visibility.Hidden;
+
+            // test
+            activeDeck = new ActiveDeck();
+            activeDeck.createDeck();
+
+            // test hand
+            testHand = new ObservableCollection<Card>();
+
+            // bind to itemscontrol
+            PlayerHandList.ItemsSource = testHand;
+            DealerHandList.ItemsSource = testHand;
         }
 
         private void OnPageLoaded(object sender, RoutedEventArgs e)
-        {
-            gameLabel.Content = GameState.GameStatus;
+        {         
         }
 
-        private void GoToTitleView(object sender, RoutedEventArgs e)
+        private void onExitButtonPress(object sender, RoutedEventArgs e)
         {
-            Console.WriteLine("Entered game view button click - go to title view");
             // retrieve navigation service to move to another page
             NavigationService nav = NavigationService.GetNavigationService(this);
             // create page
@@ -43,6 +64,14 @@ namespace testCsharp.View
             nav.Navigate(titleView);
         }
 
-        
+        private void OnHitButtonPress(object sender, RoutedEventArgs e)
+        {
+            testHand.Add(activeDeck.drawCard());
+        }
+
+        private void OnStayButtonPress(object sender, RoutedEventArgs e)
+        {
+            testHand.Clear();
+        }
     }
 }
